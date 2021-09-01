266df54612f851b2f56735e69452c4a29f3df067
Raúl <raulguzmanguerrero@outlook.com>
2021-09-01 00:29:24 -0500

    Feature basic server structure: Prettier reformatting

--- __test__/placeholder.test.js
+++ __test__/placeholder.test.js
@@ -1,8 +1,8 @@
 // This is a placeholdet test to remind you that you should probably add your own tests to this project
 // This test will always fail
 
-describe('Placeholder Test', () => {
-    test('Remember to add your tests!', () => {
+describe("Placeholder Test", () => {
+    test("Remember to add your tests!", () => {
         expect(false).toBe(true);
     });
 });
