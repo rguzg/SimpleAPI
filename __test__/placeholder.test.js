0d09bd6b927d789f1fb485b2762537279a18612a
Raúl <raulguzmanguerrero@outlook.com>
2021-09-01 00:28:13 -0500

    Feature basic server structure: Add placeholder test

---
+++ __test__/placeholder.test.js
@@ -0,0 +1,8 @@
+// This is a placeholdet test to remind you that you should probably add your own tests to this project
+// This test will always fail
+
+describe('Placeholder Test', () => {
+    test('Remember to add your tests!', () => {
+        expect(false).toBe(true);
+    });
+});
