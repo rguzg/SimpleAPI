e6d325d00ce1c4edc25555879b9c8ef0247c2f5e
Raúl <raulguzmanguerrero@outlook.com>
2021-09-01 17:51:01 -0500

    Feature Hello World endpoint: add /helloworld route to index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Routes
+app.use('/helloworld', require('./Routes/helloworld'));
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
