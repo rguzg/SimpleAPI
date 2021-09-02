db88952da9376b520c4a090588c5e59008bb0f65
Raúl <raulguzmanguerrero@outlook.com>
2021-09-01 19:09:49 -0500

    Feature Not Found middleware: Delete placeholder test

--- __test__/placeholder.test.js
+++
@@ -1,8 +0,0 @@
-// This is a placeholdet test to remind you that you should probably add your own tests to this project
-// This test will always fail
-
-describe("Placeholder Test", () => {
-    test("Remember to add your tests!", () => {
-        expect(false).toBe(true);
-    });
-});
